f2499116e9739c18ea6c10759982e55cba4057b6
Redpoll2 <dredpoll02@gmail.com>
2024-03-25 16:56:40 +0500

    Added api methods for getting exchange rates from storage by date or currency id

---
+++ Storage/Storage.Main/Controllers/StorageController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Storage.Core;
+using Storage.Database;
+
+namespace Storage.Main.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController(ICurrencyExchangeRateRepository exchangeRate) : ControllerBase
+{
+    private readonly ICurrencyExchangeRateRepository _exchangeRate = exchangeRate;
+
+    [HttpGet]
+    public IActionResult GetExchangeRatesByDate([FromQuery] DateOnly minDate, [FromQuery] DateOnly maxDate)
+    {
+        List<CurrencyExchangeRate> exchangeRates = [.. _exchangeRate.GetByDate(minDate, maxDate)];
+        return Ok(exchangeRates);
+    }
+
+    [HttpGet]
+    public IActionResult GetExchangeRatesById([FromQuery] string currencyId)
+    {
+        CurrencyExchangeRate exchangeRate = _exchangeRate.GetById(currencyId);
+        return Ok(exchangeRate);
+    }
+}
